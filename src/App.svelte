a55b20a374fefbd0307c9282ba93de49830905ee
dbertaso <dbertaso@gmail.com>
2023-01-18 20:19:37 -0400

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let name;
+	
+	function handleclick() {
+		name = 'otro nombre';
+	}
+</script>
+
+<main>
+	<button on:click={handleclick}>Cambiar nombre</button>
+	<h1>Hola {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #0d3194;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
